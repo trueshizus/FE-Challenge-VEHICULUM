f7f7a60d77c7d46dc92f3f50ce8c1ccf8cb2782f
Shizus <JesusHerrera@users.noreply.github.com>
2019-08-07 09:48:11 -0300

    Add Footer static text

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,7 +1,13 @@
 import React from "react";
+import { Link } from "@reach/router";
 
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <div>
+      <h4>Got jokes? Get paid for submitting!</h4>
+      <Link to="jokes/new">Submit Joke</Link>
+    </div>
+  );
 }
 
 export default Footer;
